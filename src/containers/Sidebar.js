488574d3634ad47780967a96d38dc4fe051f0918
guru <guru.softwaremaster@mail.ru>
2020-07-21 21:14:50 -0400

    fix api endpoint

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import SidebarComponent from '../components/Sidebar'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
